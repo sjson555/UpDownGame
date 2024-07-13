64486439d9da172fe6482d58008a7ec442e6a3b3
sungjun son <o_nj_u@naver.com>
2024-07-13 14:46:17 +0900

    업다운 앱 만들기 / 기본 세팅

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    // 앱의 화면에 들어오면 가장 처음에 실행되는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+        
+        
+    }
+    
 }
 
