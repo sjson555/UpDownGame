538c79cc18cabe42627e1107f125694baa04c0d6
sungjun son <o_nj_u@naver.com>
2024-07-15 21:48:10 +0900

    업다운 앱 만들기 / 코드 설정

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -15,34 +15,56 @@ class ViewController: UIViewController {
     // 컴퓨터가 랜덤으로 1~10 사이의 숫자를 선택해야함
     var comNumber = Int.random(in: 1...10)
     
+    // 내가 선택한 숫자를 담는 변수
+    var myNumber: Int = 1
+    
     // 앱의 화면에 들어오면 가장 처음에 실행되는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 1) mainLabel에 "선택하세요"
-        // 2) numberLabel에는 "" (공백)
         
+        // 1) mainLabel에 "선택하세요"
+        mainLabel.text = "선택하세요"
+        
+        // 2) numberLabel에는 "" (공백)
+        numberLabel.text = ""
         
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
         // 1) 버튼의 숫자를 가져와야함
+        // 반드시 각각 currentTitle을 가지고 있는 상황이니
+        // 옵셔널을 !로 강제로 벗겨도 됨
+        guard let numString = sender.currentTitle else { return }
+        
         // 2) 숫자 레이블이 변하도록 (숫자에 따라)
-        // 3) 선택한 숫자를 변수에 저장 (선택 : 저장하지 않는 방식도 있음)
+        numberLabel.text = numString
         
-        
+        // 3) 선택한 숫자를 변수에 (문자열을 숫자로 변환 후) 저장 (선택 : 저장하지 않는 방식도 있음)
+        guard let num = Int(numString) else { return }
+        myNumber = num
     }
     
     @IBAction func selectButtonTapped(_ sender: UIButton) {
         // 1) 컴퓨터의 숫자와 내가 선택한 숫자를 비교, Up / Down / Bingo (mainLabel)
-        
+        if comNumber > myNumber {
+            mainLabel.text = "Up"
+        } else if comNumber < myNumber {
+            mainLabel.text = "Down"
+        } else {
+            mainLabel.text = "Bingo😎"
+        }
         
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
         // 1) 메인레이블 "선택하세요"
-        // 2) 컴퓨터의 랜덤숫자를 다시 선택하게 해야함
-        // 3) numberLabel을 "" (빈문자열)
+        mainLabel.text = "선택하세요"
         
+        // 2) numberLabel을 "" (빈문자열)
+        numberLabel.text = ""
+        
+        // 3) 컴퓨터의 랜덤숫자를 다시 선택하게 해야함
+        comNumber = Int.random(in: 1...10)
         
     }
     
